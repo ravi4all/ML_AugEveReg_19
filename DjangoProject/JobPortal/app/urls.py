f4a50e9d6e6f0d530c8785910a694d3623c5bc05
ravi4all <dhruvasharma1989@gmail.com>
2019-09-27 18:06:41 +0530

    Add files via upload

--- DjangoProject/JobPortal/app/urls.py
+++ DjangoProject/JobPortal/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/',views.index,name='index'),
+    path('',views.index,name='index'),
+    path("viewJobs",views.viewJobs,name='viewJobs'),
+    path("devJobs",views.devJobs,name='devJobs'),
+    path("registerUser",views.registerUser,name='registerUser')
 ]
--- DjangoProject/JobPortal/app/views.py
+++ DjangoProject/JobPortal/app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pymysql
+
+connection = pymysql.connect(host="localhost",port=3306,
+user="root",database="bmpl_jobportal",autocommit=True)
+
+cursor = connection.cursor()
 
 # Create your views here.
 # def index(request):
@@ -7,3 +13,26 @@ from django.http import HttpResponse
 
 def index(req):
     return render(req, "index.html")
+
+def viewJobs(req):
+    query = "select * from jobs"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    return render(req, "viewJobs.html",{"data":data})
+
+def devJobs(req):
+    query = "select * from jobs where designation = 'Software Developer'"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    return render(req, "viewJobs.html",{"data":data})
+
+def registerUser(req):
+    username = req.POST['u_name']
+    email = req.POST['u_email']
+    pwd = req.POST['u_pwd']
+    ph = req.POST['u_ph']
+
+    query = "insert into users values (%s, %s, %s, %s)"
+    cursor.execute(query, (email,username,pwd,ph))
+
+    return render(req, 'home.html', {'name':username})
